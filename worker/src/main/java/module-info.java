6be8b820077267cca7d8ab73412a8abd06011ed0
Twonki <Leonhard.Applis@Protonmail.com>
2019-10-24 21:54:21 +0200

    removed module-info for worker for now

--- worker/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module worker {
-	requires transitive org.apache.logging.log4j;
-	//requires transitive org.apache.activemq;
-	requires java.sql;
-
-	requires activemq.all;
-	
-	exports microtope.program;
-}
